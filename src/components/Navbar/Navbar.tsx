e16f255cece4597a077f163eec615b9780f4f93c
richpham <chillpham123@gmail.com>
2026-05-25 22:22:48 -0700

    Restore Standings on desktop, place before Discord

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -23,6 +23,7 @@ export function Navbar() {
                     <Link className="navbar-nav-link hide-mobile" to="/">Home</Link>
                     <Link className="navbar-nav-link" to="/matches/">Matches</Link>
                     <Link className="navbar-nav-link" to="/results/">Results</Link>
+                    <Link className="navbar-nav-link hide-mobile" to="/standings/">Standings</Link>
                     <a className="navbar-nav-link" href="https://discord.gg/M3z9GAddUm" target="_blank" rel="noreferrer">Discord</a>
                     <Link className="navbar-nav-link hide-mobile" to="/news/">News</Link>
                     <Link className="navbar-nav-link hide-mobile" to="/gear/">Gaming Gear</Link>
