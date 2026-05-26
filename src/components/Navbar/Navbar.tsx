df117839a593ceaadcdcf92eb1188dd9d636f2cf
richpham <chillpham123@gmail.com>
2026-05-25 22:41:49 -0700

    Update Discord invite link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
                     <Link className="navbar-nav-link" to="/matches/">Matches</Link>
                     <Link className="navbar-nav-link" to="/results/">Results</Link>
                     <Link className="navbar-nav-link hide-mobile" to="/standings/">Standings</Link>
-                    <a className="navbar-nav-link" href="https://discord.gg/M3z9GAddUm" target="_blank" rel="noreferrer">Discord</a>
+                    <a className="navbar-nav-link" href="https://discord.gg/2QMfc6CG" target="_blank" rel="noreferrer">Discord</a>
                     <Link className="navbar-nav-link hide-mobile" to="/news/">News</Link>
                     <Link className="navbar-nav-link hide-mobile" to="/gear/">Gaming Gear</Link>
                     <a className="navbar-nav-link hide-mobile" href="https://lolesports.com" target="_blank" rel="noreferrer">LoL Esports</a>
